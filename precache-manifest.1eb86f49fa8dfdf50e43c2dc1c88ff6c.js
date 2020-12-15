4a3424163ca0d034fae8013d52e5ce3778dd55ca
Gilberto-Felipe <gramirez25@ucol.mx>
2020-12-14 23:07:31 -0600

    Updates

---
+++ precache-manifest.1eb86f49fa8dfdf50e43c2dc1c88ff6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61a5323e9d4a7aae7576d9a7b0edce3",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "e94b92ba0645f544f6f9",
+    "url": "/cv/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "0857b71cd24fec465a41",
+    "url": "/cv/static/css/main.2c62b281.chunk.css"
+  },
+  {
+    "revision": "e94b92ba0645f544f6f9",
+    "url": "/cv/static/js/2.90af5ca2.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/cv/static/js/2.90af5ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0857b71cd24fec465a41",
+    "url": "/cv/static/js/main.77dcc208.chunk.js"
+  },
+  {
+    "revision": "a091d0c821689d3c51ab",
+    "url": "/cv/static/js/runtime-main.5c64d99e.js"
+  }
+]);
